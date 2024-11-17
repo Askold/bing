7ed3867bf2ec6c7d30abe0e3c148b8b92f9a052d
Kirill Silonov <silonov@vtb.ru>
2024-11-17 15:42:50 +0300

    query fix

--- src/main/kotlin/ru/silonov/bing/repository/TemplateCriterioScenarioRepository.kt
+++ src/main/kotlin/ru/silonov/bing/repository/TemplateCriterioScenarioRepository.kt
@@ -1,11 +1,13 @@
 package ru.silonov.bing.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import ru.silonov.bing.model.dictionaries.ResponsibilityClass
 import ru.silonov.bing.model.linkers.TemplateCriteriaScenario
 import java.util.*
 
 interface TemplateCriterioScenarioRepository : JpaRepository<TemplateCriteriaScenario, UUID> {
 
+    @Query("select tcs from TemplateCriteriaScenario tcs where tcs.templateId.id = :templateId")
     fun findAllByTemplateId(templateId: UUID): List<TemplateCriteriaScenario>
 }
