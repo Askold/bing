1d6c5c7f6a467079e4bed948d9a76ef6ffaa65d2
Kirill Silonov <silonov@vtb.ru>
2025-03-28 13:41:35 +0300

    fact_values fix

--- src/main/kotlin/ru/silonov/bing/model/fillers/Assessment.kt
+++ src/main/kotlin/ru/silonov/bing/model/fillers/Assessment.kt
@@ -60,7 +60,7 @@ class Assessment(
     @JoinColumn(name = "object_id", nullable = false)
     var objectId: HydroObject,
 
-    @Column(name = "fact_varues")
+    @Column(name = "fact_values")
     var factValues: String? = null,
 
     @ManyToOne
